ed30fbe412e7d2dec3e4544784743d2b65708747
Ankit Kaushal <kaushalankit101010@gmail.com>
2024-05-17 23:56:18 +0530

    Update FlowBuilder.tsx

--- src/components/FlowBuilder.tsx
+++ src/components/FlowBuilder.tsx
@@ -166,7 +166,7 @@ const FlowBuilder: React.FC = () => {
       {/* // * save changes CTA */}
       <button
         onClick={onSave}
-        className="absolute top-4 right-4 bg-blue-500 text-white px-4 py-2 rounded z-[999]"
+        className="absolute duration-75 top-4 right-4 bg-blue-500 text-white px-4 py-2 rounded z-[999] hover:animate-pulse"
       >
         Save Changes
       </button>
